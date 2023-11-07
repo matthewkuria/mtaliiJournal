ea1929ff1e540f465001d6d0ff9c94b6e575e2c9
matthewkuria <mathewkwachira@gmail.com>
2023-11-07 22:12:37 +0300

    Reposition the card--stats by reducing the padding

--- src/assets/Card.jsx
+++ src/assets/Card.jsx
@@ -6,13 +6,13 @@ export default function Card(props){
          <img src={props.img} alt={props.title}
          className="card--image" />
          <div className="card--stats">
-        <div className="location--details">
-        <img src="../locationlogo.png" alt="" className="location--logo" />
-         <h5 className="country">{props.country}</h5><a href={props.link} className="card--link">View on Google Maps</a>
-        </div>
-         <h3 className="card--title">{props.title}</h3>
-         <h4>{props.date}</h4>
-         <p className="card--desc">{props.description}</p>
+            <div className="location--details">
+            <img src="../locationlogo.png" alt="" className="location--logo" />
+            <h5 className="country">{props.country}</h5><a href={props.link} className="card--link">View on Google Maps</a>
+            </div>
+            <h3 className="card--title">{props.title}</h3>
+            <h4>{props.date}</h4>
+            <p className="card--desc">{props.description}</p>
          </div>
          <hr />
        </div>
